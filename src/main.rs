638cb9cfa2652ef7b11a04d15dd4d55f80819aed
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-01-19 23:14:18 +0800

    feat(api): add /fbdl api

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 mod utils;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::Deserialize;
 
-#[tokio::main]
-async fn main() {
+#[derive(Deserialize)]
+struct ReqBody {
+    url: String,
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello from Rust!")
+}
+
+#[post("/fbdl")]
+async fn fb_dl(req_body: web::Json<ReqBody>) -> Result<impl Responder> {
     let mut token = utils::load_token();
     if token == "" {
-        println!("Token not found, fetching...");
         let page_source = utils::get_page_source().await;
         let new_token = utils::extract_token(&page_source);
         utils::save_token(&new_token);
         token = new_token;
     }
-    println!("Token found: {}", token);
-    utils::download_video("https://fb.watch/pG68jU6gWn/", &token).await;
+    println!("trying to download: {}", &req_body.url);
+    let dl_resp = utils::download_video(&req_body.url, &token).await;
+    let resp = utils::format_response(&dl_resp);
+    Ok(web::Json(resp))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(fb_dl))
+        .bind(("127.0.0.1", 6969))?
+        .run()
+        .await
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,11 +2,50 @@ use dotenv::dotenv;
 use reqwest;
 use std::{collections::HashMap, fs};
 
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct Media {
+    url: String,
+    quality: String,
+    extension: String,
+    size: u64,
+    formattedSize: String,
+    videoAvailable: bool,
+    audioAvailable: bool,
+    chunked: bool,
+    cached: bool,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct Item {
+    url: String,
+    title: String,
+    thumbnail: String,
+    duration: Option<u32>,
+    source: String,
+    medias: Vec<Media>,
+    sid: Option<String>,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct ErrorResp {
+    error: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[serde(untagged)]
+pub enum ReponseFormatResult {
+    Item(Item),
+    ErrorResp(ErrorResp),
+}
 pub async fn get_page_source() -> String {
     dotenv().ok();
     let url = std::env::var("URL").expect("NO URL FOUND IN THE CURRENT ENV");
-    let res = reqwest::get(url).await.unwrap().text().await;
-    match res {
+    let res = reqwest::get(url).await;
+    let text = match res {
+        Ok(res) => res.text().await,
+        Err(err) => Err(err),
+    };
+    match text {
         Ok(res) => res,
         Err(err) => err.to_string(),
     }
@@ -23,7 +62,7 @@ pub fn extract_token(html: &str) -> String {
 pub fn load_token() -> String {
     let token = fs::read_to_string(".token");
     match token {
-        Ok(token) => token,
+        Ok(token) => token.trim().to_string(),
         Err(_) => "".to_string(),
     }
 }
@@ -32,7 +71,7 @@ pub fn save_token(token: &str) {
     fs::write(".token", token).expect("UNABLE TO SAVE TOKEN");
 }
 
-pub async fn download_video(fb_url: &str, token: &str) {
+pub async fn download_video(fb_url: &str, token: &str) -> String {
     dotenv().ok();
     let url = std::env::var("DL_ENDPOINT").expect("NO DL_ENDPOINT FOUND IN THE CURRENT ENV");
     let cli = reqwest::Client::new();
@@ -40,8 +79,18 @@ pub async fn download_video(fb_url: &str, token: &str) {
     body.insert("token", token);
     body.insert("url", fb_url);
     let res = cli.post(url).json(&body).send().await;
-    match res {
-        Ok(res) => println!("{}", res.text().await.unwrap()),
-        Err(err) => println!("{}", err.to_string()),
+    let text = match res {
+        Ok(res) => res.text().await,
+        Err(err) => Err(err),
+    };
+    match text {
+        Ok(res) => res,
+        Err(err) => err.to_string(),
     }
+}
+
+pub fn format_response(resp: &str) -> ReponseFormatResult {
+    println!("resp: {}", resp);
+    let item: ReponseFormatResult = serde_json::from_str(resp).unwrap();
+    item
 }
